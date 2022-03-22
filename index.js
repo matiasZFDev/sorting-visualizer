ec283abd435b74e6c138c2401b4f888aae2d858a
matiaszf <cbudd333@gmail.com>
2022-03-22 16:02:43 -0300

    added: layout, design

---
+++ index.js
@@ -0,0 +1,3 @@
+const refillArrayBtn = document.getElementById('refill-array-btn');
+const arrayLengthSlider = document.getElementById('array-length-slider');
+const displaySpeedSlider = document.getElementById('speed-sliter');
